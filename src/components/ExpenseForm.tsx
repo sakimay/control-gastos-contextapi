5368ab14cb8fd1e87f6f39e62a72168f2615ffa1
Isaac <mayne.isaac@gmail.com>
2025-01-03 20:09:02 +0100

    add types

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,19 @@
 import { categories } from "../data/categories";
+import type { DraftExpense } from "../types";
+import { useState } from "react";
 import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
 
 export default function ExpenseForm() {
+
+    const [expense, setExpense] = useState<DraftExpense>({
+        expenseName: '',
+        amount: 0,
+        category: '',
+        date: new Date()
+    })
+
     return (
         <form action="" className="space-y-5" onSubmit={(e) => e.preventDefault()}>
             <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">Nuevo Gasto</legend>
@@ -17,6 +27,7 @@ export default function ExpenseForm() {
                     placeholder="Agrega el nombre del gasto"
                     className="bg-slate-100 p-2"
                     name="expenseName"
+                    value={expense.expenseName}
                 />
             </div>
             <div className="flex flex-col gap-2">
@@ -29,6 +40,7 @@ export default function ExpenseForm() {
                     placeholder="Añade cantidad del gasto"
                     className="bg-slate-100 p-2"
                     name="amount"
+                    value={expense.amount}
                 />
             </div>
             <div className="flex flex-col gap-2">
@@ -39,6 +51,7 @@ export default function ExpenseForm() {
                     name="category"
                     id="category"
                     className="bg-slate-100 p-2"
+                    value={expense.category}
                 >
                     <option value="">-- Seleccione --</option>
                     {categories.map((category) => (
@@ -52,6 +65,7 @@ export default function ExpenseForm() {
                 </label>
                 <DatePicker
                     className="bg-slate-100 p-2 border-0"
+                    value={expense.date}
                 />
             </div>
             <input type="submit" className="bg-blue-600 p-2 text-white uppercase font-bold cursor-pointer w-full" value="Agregar Gasto" />
