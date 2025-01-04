e4df829f3d65e2f4a8d5d76b32a7ebf656e0789c
Isaac <mayne.isaac@gmail.com>
2025-01-04 12:47:15 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,13 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
 
+      </div>
         {isValidBudget && (
           <main className="max-w-3xl mx-auto py-10">
-            <ExpenseList/>
             <ExpenseModal />
+            <ExpenseList/>
           </main>
         )}
-      </div>
     </>
   )
 }
